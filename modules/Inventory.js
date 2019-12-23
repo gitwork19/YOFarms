4ef96259d3ac8176f62e1e8968f3cd99b504da61
jyostna <jyostna.veerapaneni@gmail.com>
2019-12-23 09:53:57 +0530

    Adding inventory form

--- modules/Inventory.js
+++ modules/Inventory.js
@@ -1,21 +1,17 @@
 //Type your code here
 function inventorySelBoy(){
-    Form2.tbxLocation.autoFilter=true;
-    Form2.tbxLocation.filterList=["Vijayawada","Vellore","Guntur","Gudivada"];
-    Form2.fscSegment.zIndex="2";
-    var cityList=[];
-    Form2.fscSegment.setVisibility(true);
-    Form2.fscTotal.onClick=clicking;
-    var city=Form2.tbxLocation.text;
-    var name=[{"lblSegmentData":"Vijayawada"},
-              {"lblSegmentData":"Vellore"},
-              {"lblSegmentData":"Guntur"},
-              {"lblSegmentData":"Gudivada"}];
-    kony.print("name");
-    Form2.segCity.setData(name);
-    Form2.segCity.onRowClick = onRowClicking;
-    kony.print("segCity");
-    var arrayLength=name[0].length;
-    var names=name[0].lblSegmentData;
-    kony.print("names");  
+    frmInventoryReport.flxSegDeliveryBoy.setVisibility(true); 
+    frmInventoryReport.segBoy.onRowClick = selectBoyForInvetory;
+}
+
+function showInvetoryForm(){
+  frmInventoryReport.show();
+}
+
+function selectBoyForInvetory(){
+  var array = frmInventoryReport.segBoy.selectedRowItems;
+  //alert(array);
+  //kony.print(array);
+  frmInventoryReport.tbxDeliveryBoy.text=array[0].lblBoyName;
+  frmInventoryReport.flxSegDeliveryBoy.setVisibility(false);
 }
